89ccfaa97994ce65003b7d4f504fee11e054cb1f
indira konkasheva <konk0007@algonquinlive.com>
2020-01-12 18:35:41 -0500

    Create script.js file.

---
+++ script.js
@@ -0,0 +1,15 @@
+/* In the script.js, create the variable name and set its value to your name.
+ Use console.log() and the variable name to log the following message to the console: "Hello, my name is Your Name" replace Your Name with value from the name variable.
+ Create the variable number and set its value to any positive integer.
+ Using console.log() and the variable number, log the result of the following:
+     Multiply number by 2
+     Multiply the result by 5
+     Divide the result by number
+     Subtract 7 from the result */
+
+let name = 'Indira Konkasheva';
+console.log('Hello, my name is ' + name + '.');
+//or
+console.log(`Hello, my name is ${name} .`);
+let number = 5;
+console.log((number * 2 * 5) / number - 7);
